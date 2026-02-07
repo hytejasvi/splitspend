8481fd1c4271fe550d91ed1490c19b1331929385
hytejasvi <hytejasvi@gmail.com>
2026-02-07 13:57:06 +0100

    Initial Spring Boot monolith setup (Java 21, MySQL, Swagger)

---
+++ src/main/java/com/tejas/splitspend/SplitspendApplication.java
@@ -0,0 +1,13 @@
+package com.tejas.splitspend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitspendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitspendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tejas/splitspend/SplitspendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejas.splitspend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitspendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
